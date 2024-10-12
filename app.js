81ee3e29e425fbdf38de1ce994098199a32281ff
Ambika dobhal <ambikadobhal03@gmail.com>
2024-10-12 21:18:52 +0530

    app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Setting the views dir and view engine
 app.set('views', path.join(__dirname, 'views'));
